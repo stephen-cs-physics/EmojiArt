ad825d4988d88ba725b839a472922b7954c1e4b9
1317Stephen <1317stephen@gmail.com>
2022-05-07 13:27:17 +0900

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by stephen on 2022/05/07.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
