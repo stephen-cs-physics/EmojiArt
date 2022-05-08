5e75d265eb165e5d8e5d12e51af77bb100149919
1317Stephen <1317stephen@gmail.com>
2022-05-08 15:55:56 +0900

    [Lecture 10] Gesture - Background 2 : Zoom background in real time

--- EmojiArt/EmojiArtDoumentView.swift
+++ EmojiArt/EmojiArtDoumentView.swift
@@ -42,6 +42,7 @@ struct EmojiArtDoumentView: View {
             .onDrop(of: [.plainText, .url, .image], isTargeted: nil) { providers, location in
                 return drop(providers: providers, at: location, in: geometry)
             }
+            .gesture(zoomGesture())
         }
     }
     
@@ -98,7 +99,22 @@ struct EmojiArtDoumentView: View {
         CGFloat(emoji.size)
     }
     
-    @State private var zoomScale: CGFloat = 1
+    @State private var steadyStateZoomScale: CGFloat = 1
+    @GestureState private var gestureZoomScale: CGFloat = 1
+    
+    private var zoomScale: CGFloat {
+        steadyStateZoomScale * gestureZoomScale
+    }
+    
+    private func zoomGesture() -> some Gesture {
+        MagnificationGesture()
+            .updating($gestureZoomScale) { latestGestureScale, gestureZoomScale, transaction in    //ourGestureStateInOut -> gestureZoomScale
+                gestureZoomScale = latestGestureScale
+            }
+            .onEnded { gestureScaleAtEnd in
+                steadyStateZoomScale *= gestureScaleAtEnd
+            }
+    }
     
     private func doubleTapToZoom(in size: CGSize) -> some Gesture {
         TapGesture(count: 2)
@@ -112,7 +128,7 @@ struct EmojiArtDoumentView: View {
         if let image = image, image.size.width > 0, image.size.height > 0, size.width > 0, size.height > 0 {  //can use guard
             let hZoom = size.width / image.size.width
             let vZoom = size.height / image.size.height
-            zoomScale = min(hZoom, vZoom)
+            steadyStateZoomScale = min(hZoom, vZoom)
         }
     }
     
