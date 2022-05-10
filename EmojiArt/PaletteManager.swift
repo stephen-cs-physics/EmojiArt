13db6ee656cc7b5b9e2e9922dffdd015097265f7
1317Stephen <1317stephen@gmail.com>
2022-05-11 00:02:19 +0900

    [Lecture 12] Palette View : add context Menu(long tap) in palette - Manager sheet : DarkMode & Edit Mode & Close Button(with device - pad..)

--- EmojiArt/PaletteManager.swift
+++ EmojiArt/PaletteManager.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct PaletteManager: View {
     @EnvironmentObject var store: PaletteStore
-    @Environment(\.colorScheme) var colorScheme
+//    @Environment(\.colorScheme) var colorScheme
+    @Environment(\.presentationMode) var presentationMode
+    
+    @State private var editMode: EditMode = .inactive
     
     var body: some View {
         NavigationView {
@@ -17,16 +20,37 @@ struct PaletteManager: View {
                 ForEach(store.palettes) { palette in
                     NavigationLink(destination: PaletteEditor(palette: $store.palettes[palette])) {
                         VStack(alignment: .leading) {
-                            Text(palette.name).font(colorScheme == .dark ? .largeTitle : .caption)
+                            Text(palette.name).font(editMode == .active ? .largeTitle : .caption)
                             Text(palette.emojis)
                         }
+                        .gesture(editMode == .active ? tap: nil)
                     }
+                }
+                .onDelete { indexSet in
+                    store.palettes.remove(atOffsets: indexSet)
+                }
+                .onMove { indexSet, newOffset in
+                    store.palettes.move(fromOffsets: indexSet, toOffset: newOffset)
                 }
             }
             .navigationTitle("Manage Palettes")
             .navigationBarTitleDisplayMode(.inline)
-            .environment(\.colorScheme, .dark)
+            .toolbar {
+                ToolbarItem { EditButton() }
+                ToolbarItem(placement: .navigationBarLeading) {
+                    if presentationMode.wrappedValue.isPresented ,
+                       UIDevice.current.userInterfaceIdiom != .pad {
+                        Button("Close") {
+                            presentationMode.wrappedValue.dismiss()
+                        }
+                    }
+                }
+            }
+            .environment(\.editMode, $editMode)
         }
+    }
+    var tap: some Gesture {
+        TapGesture().onEnded { }
     }
 }
 
