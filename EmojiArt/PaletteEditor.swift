035b3d77ff7d13485c59987329683ccc64b1da8c
1317Stephen <1317stephen@gmail.com>
2022-05-10 23:03:04 +0900

    [Lecture 12] Palette View : add context Menu(long tap) in palette - Edit Popover : Add Emojis Section

--- EmojiArt/PaletteEditor.swift
+++ EmojiArt/PaletteEditor.swift
@@ -13,16 +13,42 @@ struct PaletteEditor: View {
     
     var body: some View {
         Form {
-            TextField("Name", text: $palette.name)  //bind!!
+            nameSection
+            addEmojiSection
         }
         .frame(minWidth: 300, minHeight: 350)
+    }
+    
+    var nameSection: some View {
+        Section(header: Text("Name")) {
+            TextField("Name", text: $palette.name)  //bind!!
+        }
+    }
+    
+    @State private var emojisToAdd = ""
+    
+    var addEmojiSection: some View {
+        Section(header: Text("Add Emojis")) {
+            TextField("", text: $emojisToAdd) //onCommit: {  }, onEditingChanged: { began in ... }
+//                .keyboardType(.webSearch)
+                .onChange(of: emojisToAdd) { emojis in
+                    addEmojis(emojis)
+                }
+        }
+    }
+    
+    func addEmojis(_ emojis: String) {
+        withAnimation {
+            palette.emojis = (emojis + palette.emojis)
+                .filter { $0.isEmoji }
+                .removingDuplicateCharacters
+        }
     }
 }
 
 struct PaletteEditor_Previews: PreviewProvider {
     static var previews: some View {
-        Text("Fix Me!!")
-//        PaletteEditor()
+        PaletteEditor(palette: .constant(PaletteStore(named: "Preview").palette(at: 4)))
             .previewLayout(.fixed(width: 300, height: 350))
     }
 }
