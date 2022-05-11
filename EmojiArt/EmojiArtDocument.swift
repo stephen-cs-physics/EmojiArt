c7e148414c464e9054bb89e3bfcdafa2d8e7ee60
1317Stephen <1317stephen@gmail.com>
2022-05-11 15:50:06 +0900

    [Lecture 13] Publisher: GCD -> Publicsher   ! >>ERROR: ProgressView() keeps spinning

--- EmojiArt/EmojiArtDocument.swift
+++ EmojiArt/EmojiArtDocument.swift
@@ -7,6 +7,7 @@
 // ViewModel
 
 import SwiftUI
+import Combine
 
 class EmojiArtDocument: ObservableObject
 {
@@ -82,26 +83,41 @@ class EmojiArtDocument: ObservableObject
         case failed(URL)
     }
     
+    private var backgroundImageFetchCancellable: AnyCancellable?
+    
     private func fetchBackgroundImageDataIfNecessary() {
         backgroundImage = nil
         switch emojiArt.background {
         case .url(let url):
             backgroundImageFetchStatus = .fetching
-            DispatchQueue.global(qos: .userInitiated).async {
-                let imageData = try? Data(contentsOf: url)   //ignore error: try? Data(~)
-                DispatchQueue.main.async { [weak self] in
-                    if self?.emojiArt.background == EmojiArtModel.Background.url(url) { // check user's not drag other images while fetching
-                        self?.backgroundImageFetchStatus = .idle
-                        if imageData != nil {
-                            self?.backgroundImage = UIImage(data: imageData!)    //purple ERROR: Publishing changes from background threads is not allowed; make sure to publish values from the main thread (via operators like receive(on:)) on model updates.
-                            //self.backgroundImage : cause it's in Closure. -> held in the heap! <NOOO!
-                        }
-                        if self?.backgroundImage == nil {
-                            self?.backgroundImageFetchStatus = .failed(url)
-                        }
-                    }
-                }
-            }
+            
+            //MARK: - [Way 2] Publisher: fetch background image
+            let session = URLSession.shared
+            let publisher = session.dataTaskPublisher(for: url)
+                .map { (data, urlResponse) in UIImage(data: data) }
+                .replaceError(with: nil)    //error -> nil
+            
+            backgroundImageFetchCancellable = publisher
+                .assign(to: \EmojiArtDocument.backgroundImage, on: self)
+            
+            
+            
+            //MARK: - [Way 1] GCD Multi thread: fetch background image
+//            DispatchQueue.global(qos: .userInitiated).async {
+//                let imageData = try? Data(contentsOf: url)   //ignore error: try? Data(~)
+//                DispatchQueue.main.async { [weak self] in
+//                    if self?.emojiArt.background == EmojiArtModel.Background.url(url) { // check user's not drag other images while fetching
+//                        self?.backgroundImageFetchStatus = .idle
+//                        if imageData != nil {
+//                            self?.backgroundImage = UIImage(data: imageData!)    //purple ERROR: Publishing changes from background threads is not allowed; make sure to publish values from the main thread (via operators like receive(on:)) on model updates.
+//                            //self.backgroundImage : cause it's in Closure. -> held in the heap! <NOOO!
+//                        }
+//                        if self?.backgroundImage == nil {
+//                            self?.backgroundImageFetchStatus = .failed(url)
+//                        }
+//                    }
+//                }
+//            }
         case .imageData(let data):
             backgroundImage = UIImage(data: data)
         case .blank:
