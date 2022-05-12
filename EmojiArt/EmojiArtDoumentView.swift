c02f7892d6be98e4538900b6ad32164366d16385
1317Stephen <1317stephen@gmail.com>
2022-05-12 22:01:54 +0900

    [Lecture 14] Document: (3) Add undo(VM) & undo manager(View)

--- EmojiArt/EmojiArtDoumentView.swift
+++ EmojiArt/EmojiArtDoumentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct EmojiArtDoumentView: View {
     @ObservedObject var document: EmojiArtDocument
     
+    @Environment(\.undoManager) var undoManager
+    
     @ScaledMetric var defaultEmojiFontSize: CGFloat = 40
     
     var body: some View {
@@ -83,12 +85,12 @@ struct EmojiArtDoumentView: View {
     private func drop(providers: [NSItemProvider], at location: CGPoint, in geometry: GeometryProxy) -> Bool {
         var found = providers.loadObjects(ofType: URL.self) { url in
             autozoom = true
-            document.setBackground(.url(url.imageURL)) //EmojiArtModel.Background.url
+            document.setBackground(.url(url.imageURL), undoManager: undoManager) //EmojiArtModel.Background.url
         }
         if !found {
             found = providers.loadObjects(ofType: UIImage.self) { image in
                 if let data = image.jpegData(compressionQuality: 1.0) {
-                    document.setBackground(.imageData(data))
+                    document.setBackground(.imageData(data), undoManager: undoManager)
                 }
             }
         }
@@ -98,7 +100,8 @@ struct EmojiArtDoumentView: View {
                     document.addEmoji(
                         String(emoji),
                         at: convertToEmojiCoordinates(location, in: geometry),
-                        size: defaultEmojiFontSize / zoomScale
+                        size: defaultEmojiFontSize / zoomScale,
+                        undoManager: undoManager
                     )
                 }
                 
