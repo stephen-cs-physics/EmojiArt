7097b908111a49b7475a248176a9c160d26b7c58
1317Stephen <1317stephen@gmail.com>
2022-05-13 22:42:59 +0900

    [Lecture 15/UIKit] iPhone background: <Way 2..> Camer/Photo Library : use Camer struct  >>ERROR[crash]: simulator can't use camera

--- EmojiArt/EmojiArtDoumentView.swift
+++ EmojiArt/EmojiArtDoumentView.swift
@@ -67,6 +67,9 @@ struct EmojiArtDoumentView: View {
                 AnimatedActionButton(title: "Paste Background", systemImage: "doc.on.clipboard") {
                     pasteBackground()
                 }
+                AnimatedActionButton(title: "Camera", systemImage: "camera") {
+                    backgroundPicker = .camera
+                }
                 if let undoManager = undoManager {
                     if undoManager.canUndo {
                         AnimatedActionButton(title: undoManager.undoActionName, systemImage: "arrow.uturn.backward") {
@@ -80,10 +83,34 @@ struct EmojiArtDoumentView: View {
                     }
                 }
             }
+            .sheet(item: $backgroundPicker) { pickerType in
+                switch pickerType {
+                case .camera: Camera(handlePickedImage: { image in handlePickerBackgroundImage(image) })
+                case .library: EmptyView()
+                }
+            }
         }
     }
     
+    private func handlePickerBackgroundImage(_ image: UIImage?) {
+        autozoom = true
+        if let imageData = image?.jpegData(compressionQuality: 1.0) {
+            document.setBackground(.imageData(imageData), undoManager: undoManager)
+        }
+        backgroundPicker = nil
+    }
+    
+    @State private var backgroundPicker: BackgroundPickerType?
+    
+    enum BackgroundPickerType: String, Identifiable{
+        case camera
+        case library
+//        var id: BackgroundPickerType { self }   //this is good when associated value is present (withoud String extend
+        var id: String { rawValue }
+    }
+    
     private func pasteBackground() {
+        autozoom = true
         if let imageData = UIPasteboard.general.image?.jpegData(compressionQuality: 1.0) {
             document.setBackground(.imageData(imageData), undoManager: undoManager)
         } else if let url = UIPasteboard.general.url?.imageURL {
